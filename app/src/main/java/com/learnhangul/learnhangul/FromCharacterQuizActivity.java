ae7c4198113d30c3a0099d31d938b032e4ed8352
Javi <bigfirelp@gmail.com>
2017-07-14 23:54:38 +0200

    Improved visuals and layout

--- app/src/main/java/com/learnhangul/learnhangul/FromCharacterQuizActivity.java
+++ app/src/main/java/com/learnhangul/learnhangul/FromCharacterQuizActivity.java
@@ -1,7 +1,6 @@
 package com.learnhangul.learnhangul;
 
 import android.content.Intent;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
